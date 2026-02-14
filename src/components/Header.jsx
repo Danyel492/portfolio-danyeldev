410527dc7bb659c9775efc85e8355f2cf3312682
Danyel492 <danyelguto@gmail.com>
2026-02-14 09:22:44 -0300

    fix: adiciona link para a home na logo no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,43 @@
-import './Header.css';
-import logo from './../assets/logo-danyeldev.png';
-import { NavLink } from 'react-router-dom';
+import "./Header.css";
+import logo from "./../assets/logo-danyeldev.png";
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <header>
-            <div className="header">
-                <img src={logo} alt="Logo" />
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
-                                Início
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/projects" className={({ isActive }) => (isActive ? 'active' : '')}>
-                                Projetos
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/contact" className={({ isActive }) => (isActive ? 'active' : '')}>
-                                Contato
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="header">
+        <NavLink to="/" style={{ textDecoration: "none", border: "none" }}>
+          <img src={logo} alt="Logo" />
+        </NavLink>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Início
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/projects"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Projetos
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Contato
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
